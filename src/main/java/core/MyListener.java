3947ceab9dda4626cf0b1fc3e67cb87cfcff965f
Kenny <kennywcorman@gmail.com>
2017-04-04 20:51:51 -0700

    Added sweet ring of fire

--- src/main/java/core/MyListener.java
+++ src/main/java/core/MyListener.java
@@ -1,19 +1,21 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Enderman;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -21,6 +23,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class MyListener implements Listener{
 	private int total = 0;
+	private Map<Location, Material> oldMaterials = new HashMap<>();
 	private Block current = null;
 	ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
     @EventHandler
@@ -43,7 +46,7 @@ public class MyListener implements Listener{
 				@Override
 				public void run() {
 	    			Bukkit.broadcastMessage("World = " + world);
-	        		world.createExplosion(x, y, z, 10, true, true);
+	        		world.createExplosion(x, y, z, 4, true, false);
 				}
 			};
 			runnable.runTaskLater(SpookCraftPlugin.plugin, 60);
@@ -53,13 +56,48 @@ public class MyListener implements Listener{
     
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent pme) {
-    	/*Block b = pme.getFrom().getBlock();
+    	Block b = pme.getFrom().getBlock();
     	if(null != current) {
     		if(!current.equals(b)) {
-    			new Location(current.getWorld(), current.getX(), current.getY() - 1, current.getZ()).getBlock().setType(Material.GLOWSTONE);
+    			onBlockChange(b, current);
     		}
     	}
-    	current = b;*/
+    	current = b;
+    }
+    
+    int radius = 7;
+    private void onBlockChange(Block prev, Block current) {
+    	// Calculate all new blocks that need to be made into lava
+    	int x = prev.getX(); int y = prev.getY(); int z = prev.getZ();
+    	List<Location> newBlocks = new ArrayList<>();
+    	int halfRad = radius/2;
+    	for(int i = -halfRad;i<=halfRad;i++) {
+    		for(int k = -halfRad;k<=halfRad;k++) {
+    			if(Math.abs(i) <= 1 && Math.abs(k) <= 1) continue;
+    			int localX = x + i;
+    			int localZ = z + k;
+    			Location loc = new Location(prev.getWorld(), localX, y, localZ);
+    			newBlocks.add(loc);
+    		}
+    	}
+    	Map<Location, Material> newMats = new HashMap<>();
+    	for(Location loc : newBlocks) {
+    		if(oldMaterials.containsKey(loc)) {
+    			newMats.put(loc, oldMaterials.get(loc));
+    			oldMaterials.remove(loc);
+    		} else {
+    			if(!loc.getBlock().getType().isSolid()) {
+        			newMats.put(loc, loc.getBlock().getType());
+            		loc.getBlock().setType(Material.FIRE);
+    			}
+    		}
+    	}
+    	Iterator<Map.Entry<Location, Material>> itr = oldMaterials.entrySet().iterator();
+    	while(itr.hasNext()) {
+    		Map.Entry<Location, Material> element = itr.next();
+    		element.getKey().getBlock().setType(element.getValue());
+    	}
+    	oldMaterials = newMats;
     }
     
     @EventHandler
